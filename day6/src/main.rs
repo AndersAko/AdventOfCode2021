f6daf8f11c1f206fafc187991bd2217a1e4e31d2
Anders Kökeritz <anders.kokeritz@tyrens.se>
2021-12-07 09:06:37 +0100

    Day 6

--- day6/src/main.rs
+++ day6/src/main.rs
@@ -2,21 +2,30 @@ use std::fs;
 
 fn main() {
     
-    let filecontents = fs::read_to_string("input_test.txt").expect("Something went wrong?");
+    let filecontents = fs::read_to_string("input.txt").expect("Something went wrong?");
     println!("{}", &filecontents.len());
     
-    let mut fishes = filecontents.split_terminator(",").map(|x| x.parse::<i32>().unwrap()).collect::<Vec<i32>>();
+    let fishes = filecontents.split_terminator(",").map(|x| x.parse::<i32>().unwrap()).collect::<Vec<i32>>();
 
+    let mut fish_ages = [0;9];
+    for &f in &fishes {
+        fish_ages[f as usize] += 1;
+    }
+    println!("Fishes: {:?} Fish_ages {:?}", &fishes, fish_ages);
+    
     for day in 1..=256 {
-        for i in 0..fishes.len() {
-            if fishes[i] > 0 {
-                fishes[i] -= 1;
+        let mut next_day_ages = [0;9];
+
+        for i in 0..fish_ages.len() {
+            if i==0 {
+                next_day_ages[6] += fish_ages[0];
+                next_day_ages[8] += fish_ages[0];
             } else {
-                fishes[i] = 6;
-                fishes.push(8);
+                next_day_ages[i-1] += fish_ages[i];
             }
         }
-        if day <= 18 { println!("After day {} ({}) {:?}", day, fishes.len(), fishes ); }
-        else { println!("After day {} ({}) ", day, fishes.len() ); }
+        fish_ages = next_day_ages;
+        let sum: u128 = fish_ages.iter().sum();
+        println!("After day {} ({}) {:?}", day, sum , fish_ages );
     }
 }
